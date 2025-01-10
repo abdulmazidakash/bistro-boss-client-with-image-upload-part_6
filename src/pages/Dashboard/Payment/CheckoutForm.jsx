20af56395f90118c533114274072d575a1f5b9ab
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-01-10 17:19:16 +0600

    70-6 running

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,10 +1,30 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { useEffect, useState } from 'react';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import useCart from '../../../hooks/useCart';
+import useAuth from '../../../hooks/useAuth';
 
 
 const CheckoutForm = () => {
 
+	const [error, setError] = useState('');
+	const [clientSecret, setClientSecret] = useState('');
+	const {user} = useAuth();
+
 	const stripe = useStripe();
 	const elements = useElements();
+	const axiosSecure = useAxiosSecure();
+	const [cart] = useCart();
+	const totalPrice = cart.reduce((total, item) => total + item.price, 0)
+
+	useEffect(()=> {
+		axiosSecure.post('/create-payment-intent', {price: totalPrice})
+			.then(res =>{
+				console.log(res.data.clientSecret);
+				setClientSecret(res.data.clientSecret);
+
+			})
+	}, [axiosSecure, totalPrice])
 
 	const handleSubmit = async(event) =>{
 		event.preventDefault();
@@ -19,31 +39,67 @@ const CheckoutForm = () => {
 			return
 		}
 
+		const {error, paymentMethod} = await stripe.createPaymentMethod({
+			type: 'card',
+			card
+		})
+
+		if(error){
+			console.log('payment error--->', error);
+			setError(error.message)
+		}
+
+		else{
+			console.log('payment method--->', paymentMethod);
+			setError('')
+		}
+
+		//confirm payment
+		const { paymentIntent, error: confirmError}  = await stripe.confirmCardPayment
+		(clientSecret, {
+			payment_method: {
+				card: card,
+				billing_details: {
+					email: user?.email || 'anonymous',
+					name: user?.displayName || 'anonymous'
+				}
+			}
+		})
+
+		if(confirmError){
+			console.log('confirm error');
+		}
+
+		else{
+			console.log('payment intent', paymentIntent);
+		}
+
 
 	}
 	return (
 		<div>
 			<form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: '16px',
-              color: '#424770',
-              '::placeholder': {
-                color: '#aab7c4',
-              },
-            },
-            invalid: {
-              color: '#9e2146',
-            },
-          },
-        }}
-      />
-      <button type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
+				<CardElement
+					options={{
+					style: {
+						base: {
+						fontSize: '16px',
+						color: '#424770',
+						'::placeholder': {
+							color: '#aab7c4',
+						},
+						},
+						invalid: {
+						color: '#9e2146',
+						},
+					},
+					}}
+				/>
+				<button className='btn btn-sm btn-info mt-4' type="submit" disabled={!stripe || !clientSecret}>Pay</button>
+
+
+				<p className="text-red-800 font-bold">{error}</p>
+				</form>
 		</div>
 	);
 };
--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,9 +1,10 @@
 import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
 //todo: add publishable key
-const stripePromise = loadStripe('');
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gatway_PK);
 
 const Payment = () => {
 	return (
@@ -11,7 +12,7 @@ const Payment = () => {
 			<SectionTitle heading={'Payment'} subHeading={'Please pay to eat'}></SectionTitle>
 			<div>
 				<Elements stripe={stripePromise}>
-
+					<CheckoutForm></CheckoutForm>
 				</Elements>
 			</div>
 		</div>
